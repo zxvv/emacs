17dc99907f300248a98d00f0944d51814ab93043
Dmitry Gutov <dgutov@yandex.ru>
2019-01-12 04:43:28 +0300

    Remove '-P 1', it's the default anyway

--- lisp/progmodes/project.el
+++ lisp/progmodes/project.el
@@ -365,7 +365,7 @@ pattern to search for."
        (status nil)
        (hits nil)
        (xrefs nil)
-       (command (format "xargs -0 -P 1 grep %s -nHe %s"
+       (command (format "xargs -0 grep %s -nHe %s"
                         (if (and case-fold-search
                                  (isearch-no-upper-case-p regexp t))
                             "-i"
